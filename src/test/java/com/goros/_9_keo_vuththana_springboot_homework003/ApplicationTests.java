53b371437b9ed59e5fe91cf23db1f25a29ac4dff
Vuththana <vuththanakeo69@gmail.com>
2026-03-28 15:27:04 +0700

    first commit

---
+++ src/test/java/com/goros/_9_keo_vuththana_springboot_homework003/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goros._9_keo_vuththana_springboot_homework003;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
